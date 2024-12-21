3f913396cc418b30f0ede1a73a98d768763e5e28
misha <frmorris590@gmail.com>
2024-12-22 00:37:33 +0300

    fix

--- tests/onliner.spec.js
+++ tests/onliner.spec.js
@@ -21,8 +21,9 @@ test ("Login page test", async({page}) => {
             await homePage.navigate('https://www.onliner.by/');
             await expect(page).toHaveTitle(/Onlíner/);
             await homePage.loginButtonClick();
+            await loginPage.login('Arnold', 'Password1');
             const response = await loginPage.login('Arnold', 'Password1');
-            expect(response.status()).toBe(400);
+            expect(response.statsu()).toBe(400)
 
             //const errorMessage = page.locator('//*[@id="auth-container"]/div/div[2]/div/form/div[3]/div').textContent()
            
